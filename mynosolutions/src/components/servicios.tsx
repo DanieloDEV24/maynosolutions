5c70ebce1d56f902977ce0fb564f56ab19e77d75
DanieloDEV24 <danielruizdeveloper@gmail.com>
2025-10-08 22:08:07 +0200

    card servicios

--- mynosolutions/src/components/servicios.tsx
+++ mynosolutions/src/components/servicios.tsx
@@ -1,19 +1,32 @@
+import { IconoFlyer } from "./icons/iconoFlyer"
+import {  IconoRedSocial } from "./icons/iconoRedSocial"
+import { IconoCodigo } from "./icons/iconoCodigo"
+
 export const Servicios = () => {
     return (
         <div className="servicios">
             <h1 className="text-4xl text-white font-bold mb-4">NUESTROS SERVICIOS</h1>
-            <div className="cards flex justifi-center gap-1 flex-wrap">
+            <div className="cards flex justify-center gap-1 flex-wrap">
                 <div className="card card-cartel">
-                    <h3 className="text-xl text-white font-bold">Carteles y Contenido Digital</h3>
+                    <div className="flex align-center gap-2" style={{width: "100%"}}>
+                        <IconoFlyer/>
+                        <h3 className="text-xl text-white font-bold" style={{width: "100%"}}>Carteles y Contenido Digital</h3>
+                    </div>
                     <p></p>
                 </div>
 
                 <div className="card card-redes">
-                    <h3 className="text-xl text-white font-bold">Presencia Digital y Redes Sociales</h3>
+                    <div className="flex align-center gap-2" style={{width: "100%"}}>
+                        <IconoRedSocial/>
+                        <h3 className="text-xl text-white font-bold" style={{width: "100%"}}>Presencia Digital y Redes Sociales</h3>
+                    </div>
                 </div>
 
                 <div className="card card-web">
-                    <h3 className="text-xl text-white font-bold">Aplicaciones Web</h3>
+                    <div className="flex align-center gap-2" style={{width: "100%"}}>
+                        <IconoCodigo/>
+                        <h3 className="text-xl text-white font-bold" style={{width: "100%"}}>Aplicaciones Web</h3>
+                    </div>
                 </div>      
             </div>
         </div>
